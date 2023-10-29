d832c2a0ab62d01303a4fbc7b3f88be38bbe473a
Fernando <fernan.sax@hotmail.com>
2023-10-29 20:18:28 +0100

    remove env

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,8 +1,8 @@
 
 
 export const ENV = {
-    SERVER_HOST : process.env.NEXT_PUBLIC_SERVER_HOST,
-    API_URL : process.env.NEXT_PUBLIC_API_URL ,
+    SERVER_HOST : "https://ecommerceserver-strapi-proj-1.up.railway.app",
+    API_URL : "https://ecommerceserver-strapi-proj-1.up.railway.app/api",
     ENDPOINTS : {
         AUTH : {
             REGISTER : "auth/local/register",
@@ -21,3 +21,4 @@ export const ENV = {
     CART : 'cart',
     STRIPE_TOKEN : process.env.NEXT_PUBLIC_STRIPE ,
 }
+
